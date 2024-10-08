901b60c419f4a6a8ab53470eedbc231bec2e0e7e
AstrocanX <astrocanx@gmail.com>
2024-10-08 07:00:23 +0300

    FirstCommitEver

---
+++ src/main/java/com/astra/astrosfristplugin/AstrosFristPlugin.java
@@ -0,0 +1,17 @@
+package com.astra.astrosfristplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AstrosFristPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
